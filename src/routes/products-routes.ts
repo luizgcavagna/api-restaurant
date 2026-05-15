0af3ada38c1d60dc1e59430660cd28da8aa41a50
Luiz G Cavagna <luizcavagna@gmail.com>
2026-05-15 17:10:10 -0300

    products

--- src/routes/products-routes.ts
+++ src/routes/products-routes.ts
@@ -4,6 +4,9 @@ import { ProductController } from "@/controllers/products-controller";
 const productsRoutes = Router();
 const productsController = new ProductController()
 
-productsRoutes.get("/", productsController.index)
+productsRoutes.get("/", productsController.index);
+productsRoutes.post("/", productsController.create);
+productsRoutes.put("/:id", productsController.update);
+productsRoutes.delete("/:id", productsController.remove);
 
 export { productsRoutes };
